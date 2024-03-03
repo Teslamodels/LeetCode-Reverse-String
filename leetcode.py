a73372d7b4b8abf85bd9c8368824b99453feb72f
Azizbek <149267206+Teslamodels@users.noreply.github.com>
2024-03-03 21:49:00 +0500

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1,33 +1,25 @@
-# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+# Write a function that reverses a string. The input string is given as an array of characters s.
 
-# Given a string s, return true if it is a palindrome, or false otherwise.
+# You must do this by modifying the input array in-place with O(1) extra memory.
 
  
 
 # Example 1:
 
-# Input: s = "A man, a plan, a canal: Panama"
-# Output: true
-# Explanation: "amanaplanacanalpanama" is a palindrome.
+# Input: s = ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
 # Example 2:
 
-# Input: s = "race a car"
-# Output: false
-# Explanation: "raceacar" is not a palindrome.
-# Example 3:
-
-# Input: s = " "
-# Output: true
-# Explanation: s is an empty string "" after removing non-alphanumeric characters.
-# Since an empty string reads the same forward and backward, it is a palindrome.
-
+# Input: s = ["H","a","n","n","a","h"]
+# Output: ["h","a","n","n","a","H"]
+ 
 
 # Solution
 
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = ''.join(e for e in s if e.isalnum()).lower()
-        return s==s[::-1]
-
+    def reverseString(self, s: list[str]) -> None:
+        s.reverse()
+        return s
 solution1 = Solution()
-print(solution1.isPalindrome(s = " "))
+print(solution1.reverseString(s = ["h","e","l","l","o"]))
+        
